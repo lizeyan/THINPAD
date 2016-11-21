9b5f914290b1bfe092be6b68bcf4fca50c1f5cd7
lixiaohan <lixh14@mails.tsinghua.edu.cn>
2016-11-21 22:35:18 +0800

    extending control signal completed. example.vhd added for reference

--- ControlUnit.vhd
+++ ControlUnit.vhd
@@ -23,26 +23,34 @@ use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 
 entity ControlUnit is
-    Port ( AluOp : out STD_LOGIC_VECTOR(3 downto 0);
+    Port ( 
+           -- IF generate
+           ExDigitsOp : out STD_LOGIC_VECTOR(2 downto 0);
+           ExSignOp : out STD_LOGIC;
+           PC_SRCOP : OUT STD_LOGIC_VECTOR(1 DOWNTO 0); -- zonghe
+           -- ID
+           AluOp : out STD_LOGIC_VECTOR(3 downto 0);
            AMuxOp : out STD_LOGIC_VECTOR(3 downto 0);
            BMuxOp : out STD_LOGIC_VECTOR(2 downto 0);
            DirOp : out STD_LOGIC_VECTOR(2 downto 0);
-           ExDigitsOp : out STD_LOGIC_VECTOR(2 downto 0);
-           ExSignOp : out STD_LOGIC;
            IDPCOp : out STD_LOGIC_VECTOR(1 downto 0);
-           RamEn: out STD_LOGIC_VECTOR(1 downto 0);
-           RamRWOp : out STD_LOGIC_VECTOR(1 downto 0);
            RegWrbOp : out STD_LOGIC_VECTOR(1 downto 0);
            RXTOp : out STD_LOGIC_VECTOR(1 downto 0);
-           
+           SWSrc : out STD_LOGIC; -- 0 rx, 1 ry
+           RamRWOp : out STD_LOGIC; -- 0 read, 1 write
+           BTBOP : out STD_LOGIC; -- is jumping ins(1) or not(0)
+                -- ENABLE  complex
            EXE_RFOp : out STD_LOGIC_VECTOR(1 downto 0);
            ID_RFOp : out STD_LOGIC_VECTOR(1 downto 0);
            IF_RFOp : out STD_LOGIC_VECTOR(1 downto 0);
            MEM_RFOp : out STD_LOGIC_VECTOR(1 downto 0);
            PC_RFOp : out STD_LOGIC_VECTOR(1 downto 0);
+           -- EXE
+           IF_EN : out STD_LOGIC; -- put it in pc_rf. !!! NOT COMPLETED
            
+           IF_Ins : in STD_LOGIC_VECTOR(15 downto 0);
            IF_RF_OP : in STD_LOGIC_VECTOR(4 downto 0);
-           ID_RF_OP : in STD_LOGIC_VECTOR(4 downto 0);
+           ID_RF_OP : in STD_LOGIC_VECTOR(4 downto 0); 
            ID_RF_Rd : in STD_LOGIC_VECTOR(3 downto 0);
            EXE_RF_OP : in STD_LOGIC_VECTOR(4 downto 0);
            EXE_RF_Rd : in STD_LOGIC_VECTOR(3 downto 0)
@@ -52,7 +60,70 @@ entity ControlUnit is
 end ControlUnit;
 
 architecture Behavioral of ControlUnit is
-
+    
 begin
-
+    process(IF_Ins)
+	-- EX Digits Op
+	-- 000 10:0
+	-- 001 7:0
+	-- 011 4:0
+	-- 010 4:2
+	-- 110 3:0
+	-- others Z... (ILLEGAL)
+    begin
+        case IF_Ins(15 downto 11) is
+            when "01001" => -- addiu
+            	ExDigitsOp <= "001";
+           		ExSignOp <= '1';
+            when "01000" => -- addiu3
+            	ExDigitsOp <= "110";
+           		ExSignOp <= '1';
+            when "01100" => --addsp, bteqz, mtsp
+            	if(IF_Ins(10 downto 8)="000" or IF_Ins(10 downto 8)="011") then
+            		ExDigitsOp <= "001";
+           			ExSignOp <= '1';
+           		else
+           		end if;
+            when "00000" => --addsp3
+            	ExDigitsOp <= "001";
+           		ExSignOp <= '1';
+            when "00010" => -- b
+            	ExDigitsOp <= "000";
+           		ExSignOp <= '1';
+            when "00100" => -- beqz
+            	ExDigitsOp <= "001";
+           		ExSignOp <= '1';
+            when "00101" => -- bnez
+            	ExDigitsOp <= "001";
+           		ExSignOp <= '1';
+            when "01110" => -- cmpi
+            	ExDigitsOp <= "001";
+           		ExSignOp <= '1';
+            when "01101" => -- li
+            	ExDigitsOp <= "001";
+           		ExSignOp <= '0';
+            when "10011" => -- lw
+            	ExDigitsOp <= "011";
+           		ExSignOp <= '1';
+            when "10010" => -- lw_sp
+            	ExDigitsOp <= "001";
+           		ExSignOp <= '1';
+            when "00110" => -- sll, sra
+            	ExDigitsOp <= "010";
+           		ExSignOp <= '0';
+            when "01010" => -- slti
+            	ExDigitsOp <= "001";
+           		ExSignOp <= '1';
+            when "11011" => -- sw
+            	ExDigitsOp <= "011";
+           		ExSignOp <= '1';
+            when "11010" => -- swsp
+            	ExDigitsOp <= "001";
+           		ExSignOp <= '1';
+            when others =>
+            	ExDigitsOp <= "111";
+           		ExSignOp <= '1';
+        end case;
+    end process;
+    
 end Behavioral;
