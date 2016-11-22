8f80fa3554e13694028ba015a4942da72defb6de
lizeyan <zy-li14@hotmail.com>
2016-11-22 20:35:08 +0800

    完成ALUOP和Rd的生成

--- ControlUnit.vhd
+++ ControlUnit.vhd
@@ -25,15 +25,15 @@ use IEEE.std_logic_unsigned.all;
 entity ControlUnit is
     Port ( 
            -- IF generate
-           ExDigitsOp : out STD_LOGIC_VECTOR(2 downto 0);
+           ExDigitsOp : out STD_LOGIC_VECTOR(2 downto 0); --传输到ExtendModule
            ExSignOp : out STD_LOGIC;
-           PC_SRCOP : OUT STD_LOGIC_VECTOR(1 DOWNTO 0); -- zonghe
+           PC_SRCOP : out STD_LOGIC_VECTOR(1 DOWNTO 0); -- --传送到PC寄存器
            -- ID
-           AluOp : out STD_LOGIC_VECTOR(3 downto 0);
-           AMuxOp : out STD_LOGIC_VECTOR(3 downto 0);
-           BMuxOp : out STD_LOGIC_VECTOR(2 downto 0);
-           DirOp : out STD_LOGIC_VECTOR(2 downto 0);
-           IDPCOp : out STD_LOGIC_VECTOR(1 downto 0);
+           AluOp : out STD_LOGIC_VECTOR(3 downto 0); -- 保存到ID_RF
+           AMuxOp : out STD_LOGIC_VECTOR(3 downto 0); --
+           BMuxOp : out STD_LOGIC_VECTOR(2 downto 0); --
+           DirOp : out STD_LOGIC_VECTOR(2 downto 0); -- 传输到DirectionModule
+           IDPCOp : out STD_LOGIC_VECTOR(1 downto 0); 
            
            RegWrbOp : out STD_LOGIC_VECTOR(1 downto 0);
            RXTOp : out STD_LOGIC_VECTOR(1 downto 0);
@@ -51,6 +51,7 @@ entity ControlUnit is
            
            IF_Ins : in STD_LOGIC_VECTOR(15 downto 0);
            IF_RF_OP : in STD_LOGIC_VECTOR(4 downto 0);
+			  IF_RF_ST: in STD_LOGIC_VECTOR (15 downto 0); -- IF段寄存器中保存的，指令的内容。因为有的指令需要判断funct字段
            ID_RF_OP : in STD_LOGIC_VECTOR(4 downto 0); 
            ID_RF_Rd : in STD_LOGIC_VECTOR(3 downto 0);
            EXE_RF_OP : in STD_LOGIC_VECTOR(4 downto 0);
@@ -63,6 +64,136 @@ end ControlUnit;
 architecture Behavioral of ControlUnit is
     
 begin
+	-- 产生目标寄存器选项的控制码，之后需要将其传输到DirectionModule
+	-- 产生ALU操作码，之后需要将其保存到ID_RF
+	-- 使用ID段得到的IF_RD_OP
+	process (IF_RF_OP)
+		variable op: STD_LOGIC_VECTOR (4 downto 0);
+		variable dir: STD_LOGIC_VECTOR (2 downto 0); -- 我太懒
+	begin
+		op := if_rf_st (15 downto 11);
+		case op is
+            when "01001" => -- addiu
+					aluop <= "0000";
+					dir := "000";
+            when "01000" => -- addiu3
+					aluop <= "0000";
+					dir := "001";
+            when "01100" => --addsp, bteqz, mtsp
+					case if_rf_st (10 downto 8) is
+						when "011" => -- addsp
+							aluop <= "0000";
+							dir := "010";
+						when "000" => --btnez
+							aluop <= "0110";
+							dir := "111";
+						when "100" => --mtsp
+							aluop <= "0011";
+							dir := "010";
+						when others =>
+							aluop <= "1111";
+							dir := "111";
+					end case;
+            when "00000" => --addsp3
+					aluop <= "0000";
+					dir := "000";
+            when "00010" => -- b
+					aluop <= "0000";
+					dir := "111";
+            when "00100" => -- beqz
+					aluop <= "0110";
+					dir := "111";
+            when "00101" => -- bnez
+					aluop <= "0110";
+					dir := "111";
+            when "01110" => -- cmpi
+					aluop <= "0110";
+					dir := "100";
+            when "01101" => -- li
+					aluop <= "0011";
+					dir := "000";
+            when "10011" => -- lw
+					aluop <= "0000";
+					dir := "001";
+            when "10010" => -- lw_sp
+					aluop <= "0000";
+					dir := "000";
+            when "00110" => -- sll, sra
+					case if_rf_st(1 downto 0) is
+					when "00" =>
+						aluop <= "0111";
+						dir := "000";
+					when "11" =>
+						aluop <= "0100";
+						dir := "000";
+					when others =>
+						aluop <= "1111";
+						dir := "111";
+					end case;
+            when "01010" => -- slti
+					aluop <= "0001";
+					dir := "100";
+				when "01111" => --move
+					aluop <= "0011";
+					dir := "000";
+            when "11011" => -- sw
+					aluop <= "0000";
+					dir := "111";
+            when "11010" => -- swsp
+					aluop <= "0000";
+					dir := "111";
+				when "11100" => 
+					case if_rf_st(1 downto 0) is
+						when "01" => --addu
+							aluop <= "0000";
+							dir := "011";
+						when "11" => --subu
+							aluop <= "0001";
+							dir := "011";
+						when others =>
+							aluop <= "1111";
+							dir := "111";
+					end case;
+				when "11101" =>
+					case if_rf_st(4 downto 0) is
+						when "01100" => -- and
+							aluop <= "0011";
+							dir := "000";
+						when "01010" => --cmp
+							aluop <= "0110";
+							dir := "100";
+						when "11101" => --or
+							aluop <= "0010";
+							dir := "000";
+						when "00100" => -- sllv
+							aluop <= "0101";
+							dir := "001";
+						when "00000" => --mfpc
+							aluop <= "0010";
+							dir := "000";
+						when others =>
+							aluop <= "1111";
+							dir := "111";
+					end case;
+				when "11110" =>
+					aluop <= "0010";
+					case if_rf_st(0) is
+						when '0' => -- mfih
+							dir := "000";
+						when '1' => -- mtih
+							dir := "110";
+						when others =>
+							dir := "111";
+					end case;
+            when others =>
+					aluop <= "1111";
+					dir := "111";
+			end case;
+			dirop <= dir;
+	end process;
+
+	-- 产生立即数扩展的控制信号
+	-- 使用IF段的得到的IF_INS
     process(IF_Ins)
 	-- EX Digits Op
 	-- 000 10:0
@@ -127,4 +258,57 @@ begin
         end case;
     end process;
     
+	 
+	-- template
+	process
+	variable op: STD_LOGIC_VECTOR (4 downto 0);
+	begin
+		op := if_rf_st (15 downto 11);
+		case op is
+            when "01001" => -- addiu
+            when "01000" => -- addiu3
+            when "01100" => 
+					case if_rf_st (10 downto 8) is
+						when "011" => -- addsp
+						when "000" => --btnez
+						when "100" => --mtsp
+						when others =>
+					end case;
+            when "00000" => --addsp3
+            when "00010" => -- b
+            when "00100" => -- beqz
+            when "00101" => -- bnez
+            when "01110" => -- cmpi
+            when "01101" => -- li
+            when "10011" => -- lw
+            when "10010" => -- lw_sp
+            when "00110" => -- sll, sra
+					case if_rf_st(1 downto 0) is
+						when "00" =>
+						when "11" =>
+						when others =>
+					end case;
+            when "01010" => -- slti
+				when "01111" => --move
+            when "11011" => -- sw
+            when "11010" => -- swsp
+				when "11100" => 
+					case if_rf_st(1 downto 0) is
+						when "01" => --addu
+						when "11" => --subu
+						when others =>
+					end case;
+				when "11101" =>
+					case if_rf_st(4 downto 0) is
+						when "01100" => -- add
+						when "01010" => --cmp
+						when "11101" => --or
+						when "00100" => -- sllv
+						when "00000" => --mfpc
+						when others =>
+					end case;
+				when "11110" => --mfih and mtih
+            when others =>
+			end case;
+	end process;
 end Behavioral;
